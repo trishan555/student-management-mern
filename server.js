60446a14595d9412424ff267e6a06adc75fa6e19
TrishLeaf <trishanp96@gmail.com>
2022-06-22 19:08:39 +0530

    create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+require('dotenv').config()
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+const URL = process.env.MONGODB_URL
+
+mongoose.connect(URL)
+
+const connection = mongoose.connection
+connection.once('open', () => {
+    console.log('Mongodb connection success!')
+})
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+
+const PORT = process.env.PORT || 8070
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port ${PORT}`);
+})
